a69765048c439a2edeba659d65210568d8103cc3
softeyes <767733103@qq.com>
2018-01-22 22:21:22 +0800

    优化进度显示

--- trash/js/main.js
+++ trash/js/main.js
@@ -1,89 +1,99 @@
-//var footer = document.getElementById('footer');
-//var lis = footer.getElementsByTagName('li');
-//for(var i=0; i<lis.length; i++) {
-//	lis[i].addEventListener('tap',function() {
-//		
-//		for(var j=0; j<lis.length; j++) {
-//			lis[j].className = '';
-//		}
-//		this.className = 'current';
-//	})
-//}
 
-//for(var i=0; i<document.getElementsByTagName('a').length; i++) {
-//	document.getElementsByTagName('a')[0].addEventListener('tap',function(e) {
-//		e.preventDefault(); 
-//	})
-//}
-
-var oldPer = 0.8;
-
-function CreateBar(num, parentNode, per, ms) {
-    parentNode.innerHTML = '';
-    getDom('#soft-info-perText').innerHTML = '0%';
-    this.number = num;
-    this.per = num * per;
+/*创建进度条，数量100*/
+function CreateSchedule() {
+    this.number = 100;
     this.rotateNum = 360 / this.number;
-    this.parentNode = parentNode;
-    var that = this;
-    var timer = null;
-    clearInterval(timer);
-    for (var i = 0; i < this.number; i++) {
-        var infoSchedule = document.createElement('div');
-        infoSchedule.className = 'soft-info-schedule-sm';
-        infoSchedule.style.webkitTransform = 'rotate(' + i * that.rotateNum + 'deg)';
-        that.parentNode.appendChild(infoSchedule)
+    this.parentNode = getDom('.soft-info-schedule')[0];
+    for(var i=0; i<this.number; i++) {
+        var smSchedule = document.createElement('div');
+        smSchedule.className = 'soft-info-schedule-tra';
+        smSchedule.style.webkitTransform = 'rotate(' + i * this.rotateNum + 'deg)';
+        this.parentNode.appendChild(smSchedule);
     }
-    var needShow = Math.round(that.per);
-    var beginShow = 0;
-    var countNum = needShow / that.per; 
-    console.log(needShow)
-    var countText = 0;
-    if(per == 0) {
-        console.log(oldPer)
-        thisPer = oldPer * 100;
-        timer = setInterval(function() {
-            if(thisPer >=0 ){
-                getDom('#soft-info-perText').innerHTML = thisPer + '%';
-                that.parentNode.children[20].className = 'soft-info-schedule-sm';
-            }
-            thisPer--;
-            console.log(thisPer)
-        },100)
-    }else if(per == 1){
+}
+new CreateSchedule();
 
+/*初始化进度条进度*/
+/*
+    per 百分比参数
+    ele 进度条元素
+    ms  控制进度加载速度
+    有三种情况
+      1. 为0 
+      2. 为1
+      3. 0~1
+    还需要实时记录进度，不能每次都让进度条重新加载，影响观感和代码的执行速度   
+*/ 
+/*监测上一次的进度百分比*/
+var oldPer = 0;
+function initSchedlue(per,ele,ms) {
+    if(per == 0) {
+        loadingAnimate(100,per,ele,ms)
+    }else if(per == 1) {
+        loadingAnimate(100,per,ele,ms)
     }else {
-    timer = setInterval(function() {
-        countText += countNum;
-        beginShow++;   
-        if(beginShow < needShow){
-            getDom('#soft-info-perText').innerHTML = countText.toFixed(1) + '%';
-            that.parentNode.children[beginShow].className = 'soft-info-schedule-white'
-        }
-    },ms)
-    console.log(1);
+        loadingAnimate(100,per,ele,ms)
+    }
+    
+}
+
+/*动画执行*/
+function loadingAnimate(num,per,ele,ms) {
+    let timer1 = null;
+    let timer2 = null;
+    per = per * 100; 
+    clearInterval(timer1);
+    clearInterval(timer2);
+    console.log(per,oldPer)
+    /*增加*/
+    if(per > oldPer) {
+        timer1 = setInterval(function() {
+            if(per >= oldPer) {
+                if(oldPer == 100) {
+                    oldPer = 99;
+                    getDom('#soft-info-perText').innerHTML = '100%';
+                    ele.children[99].className = 'soft-info-schedule-white';
+                    clearInterval(timer1);
+                    return;                    
+                }
+                getDom('#soft-info-perText').innerHTML = oldPer + '%';
+                ele.children[oldPer].className = 'soft-info-schedule-white';
+            }else {
+                oldPer = per;
+                clearInterval(timer1);
+                return;
+            }
+            oldPer++;
+        },ms);
+     }
+    else {
+        /*减少*/
+        timer2 = setInterval(function() {
+            if(oldPer >= per) {
+                getDom('#soft-info-perText').innerHTML = oldPer + '%';
+                ele.children[oldPer].className = 'soft-info-schedule-tra';
+            }else {
+                oldPer = per;
+                clearInterval(timer2); 
+                return;   
+            }
+            oldPer--;
+        },ms)
     }
 }
 
+
+/*点击事件 获得input输入值*/
 getDom('#soft-info-changePer').addEventListener('tap',function() {
-    throttle(chnageValue)
+    changeValue()
 });
 
-function chnageValue() {
-    getDom('.soft-info-schedule')[0].innerHTML = '';
+function changeValue() {
+    let ele = getDom('.soft-info-schedule')[0];
     getDom('#soft-info-perText').innerHTML = '0%';
     var value = getDom('#soft-info-inputPer').value;
-    // value = value.split(',');
-    // oldPer = value-0;
-    console.log(oldPer)
-    var createBar2 = new CreateBar(100, getDom('.soft-info-schedule')[0],value-0,10)
+    initSchedlue(value,ele,30);
 }
 
-function throttle(method,context){
-    clearTimeout(method.tId);
-    method.tId=setTimeout(function(){
-        method.call(context);
-    },10);
-}
 
-var createBar1 = new CreateBar(100, getDom('.soft-info-schedule')[0], 0.8, 20);
+
