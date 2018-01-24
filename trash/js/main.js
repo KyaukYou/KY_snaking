787cbae71780ceec8f9cb983aec70c1848af8b61
softeyes <767733103@qq.com>
2018-01-24 20:41:37 +0800

    添加登录界面

--- trash/js/main.js
+++ trash/js/main.js
@@ -26,7 +26,15 @@ function CreateSchedule(num, ele, clas) {
 var oldPer = 0;
 var oldTemp = 0;
 
+/*
+    因为进度条无法精确到小数点
+    所以这里需要保存一个真实的
+    温度数据
+*/
+var trueTemp = 0;
+
 function initSchedlue(per, ele, ms, clas1, clas2, temp, type) {
+    per = per || 0;
     if (per == 0) {
         loadingAnimate(100, per, ele, ms, clas1, clas2, temp, type)
     } else if (per == 1) {
@@ -96,14 +104,15 @@ function loadingAnimate(num, per, ele, ms, clas1, clas2, innerText, type) {
         /*增加*/
         if (per > oldTemp) {
             timer3 = setInterval(function () {
+                console.log(oldTemp)
                 if (per >= oldTemp) {
-                    getDom(innerText).innerHTML = oldTemp+1 + '%';
+                    getDom(innerText).innerHTML = oldTemp+1 + '℃';
                     ele.getElementsByTagName('div')[oldTemp].className = clas2;
-                    console.log(ele.getElementsByTagName('div')[oldTemp])
+                    // console.log(ele.getElementsByTagName('div')[oldTemp])
                 } else {
                     oldTemp = per;
                     var a = oldTemp;
-                    getDom(innerText).innerHTML = a+1 + '%';
+                    getDom(innerText).innerHTML = trueTemp + '℃';
                     ele.getElementsByTagName('div')[oldTemp].className = clas2;
                     clearInterval(timer3);
                     return;
@@ -120,15 +129,15 @@ function loadingAnimate(num, per, ele, ms, clas1, clas2, innerText, type) {
                     if(oldTemp <=0 ) {
                         oldTemp = 0;
                         clearInterval(timer4);
-                        getDom(innerText).innerHTML = 0 + '%';
+                        getDom(innerText).innerHTML = 0 + '℃';
                         ele.getElementsByTagName('div')[oldTemp].className = clas1;
                         return;
                     }
-                    getDom(innerText).innerHTML = oldTemp+1 + '%';
+                    getDom(innerText).innerHTML = oldTemp+1 + '℃';
                     ele.getElementsByTagName('div')[oldTemp].className = clas1;
                 } else {
                     oldTemp = per;
-                    getDom(innerText).innerHTML = oldTemp+1 + '%';
+                    getDom(innerText).innerHTML = trueTemp + '℃';
                     ele.getElementsByTagName('div')[oldTemp].className = clas2;
                     clearInterval(timer4);
                     return;
@@ -141,15 +150,19 @@ function loadingAnimate(num, per, ele, ms, clas1, clas2, innerText, type) {
 
 
 /*点击事件 获得input输入值*/
-getDom('#soft-info-changePer').addEventListener('tap', function () {
-    changeValue()
-});
+// getDom('#soft-info-changePer').addEventListener('tap', function () {
+//     changeValue()
+// });
 
 function changeValue() {
     var ele = getDom('.soft-info-schedule')[0];
     var value = getDom('#soft-info-inputPer').value;
+    var value1 = getDom('#soft-info-inputPer1').value;
+    trueTemp = value1;
     value = Math.round(parseFloat(value) * 100) / 100;
+    value1 = Math.round(parseFloat(value1) * 100) / 100;
     initSchedlue(value, ele, 20, 'soft-info-schedule-white', 'soft-info-schedule-tra', '#soft-info-perText',1);
+    initSchedlue(value1, getDom('.soft-info-temp')[0], 30,'soft-info-temp-tra','soft-info-temp-orange','#soft-info-temp',2);
 }
 
 
@@ -174,5 +187,15 @@ getDom('#soft-info-bottom').addEventListener('tap', function () {
 
 /*滚动函数*/
 function scrollInfo(index) {
-    getDom('.soft-info-log')[0].style.webkitTransform = "translateY(" + -index * 33.33333333 + "%)";
+    getDom('.soft-info-log')[0].style.transform = "translateY(" + -index * 33.33333333 + "%)";
+}
+
+/*ajax获取数据检测，每隔3s检测一次*/
+function softAjax() {
+    //创建ajax对象
+    var xhr = new XMLHttpRequest();
+}
+
+function ajaxGetInfo() {
+
 }
